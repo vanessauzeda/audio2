ded331149ba0be38691c52cc052afc82c35bcc81
Vanessa Uzêda <bispovanessa21@gmail.com>
2022-09-03 15:27:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let audio = document.getElementById("audio");
+
+// function loop(){
+
+// }
+
+// function back(){
+    
+// }
+
+function pause(){
+audio.pause();
+}
+
+function play(){
+    audio.play();
+    
+}
+function next(){
+
+}
+
+// function random(){
+
+// }
+
+function volume_menos(){
+audio.volume -= 0.2;
+}
+
+function volume_mais(){
+    audio.volume += 0.2;
+    }
